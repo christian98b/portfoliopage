e34850e28980a1fbe5e105a1ce79933814f67d82
Christian Bruns <christianbruns1039@gmail.com>
2026-04-17 18:36:08 +0200

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://christian98b.github.io/portfoliopage",
+  base: "/portfoliopage",
+});
